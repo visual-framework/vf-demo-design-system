ce9c02804bd40bf10c408c959d634a0e53d4cebd
Ken Hawkins <khawkins98@gmail.com>
2019-07-22 11:30:57 +0200

    Update name

--- src/site/_data/siteConfig.js
+++ src/site/_data/siteConfig.js
@@ -1,7 +1,7 @@
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "Demo design system",
+    title: "Design system boilerplate",
     short_description: 'This demonstrates how a design system can be built using the the Visual Framework component system.',
     url: "https://visual-framework.github.io/vf-demo-design-system/",
     author: "Visual Framework system",
